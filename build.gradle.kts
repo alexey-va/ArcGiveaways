3f35c95ae5d2c4b4985e61d795de9a0a54bb84f8
alexey-va <freedeeml@gmail.com>
2026-09-09 21:56:28 +0300

    Adopt Core 2.7.5 automatic native dialog history

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ru.ruscrafting"
-version = "0.1.12"
+version = "0.1.13"
 description = "Cross-server proximity giveaways for RusCrafting"
 
 val integrationTestSourceSet = sourceSets.create("integrationTest") {
@@ -44,12 +44,12 @@ kotlin { jvmToolchain(25) }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("ru.ruscrafting.arc:arc-core:2.7.4")
-    implementation("ru.ruscrafting.arc:arc-core-logging:2.7.4")
-    implementation("ru.ruscrafting.arc:arc-core-paper:2.7.4")
-    implementation("ru.ruscrafting.arc:arc-core-menu:2.7.4")
-    implementation("ru.ruscrafting.arc:arc-core-paper-menu:2.7.4")
-    implementation("ru.ruscrafting.arc:arc-core-redis:2.7.4")
+    implementation("ru.ruscrafting.arc:arc-core:2.7.5")
+    implementation("ru.ruscrafting.arc:arc-core-logging:2.7.5")
+    implementation("ru.ruscrafting.arc:arc-core-paper:2.7.5")
+    implementation("ru.ruscrafting.arc:arc-core-menu:2.7.5")
+    implementation("ru.ruscrafting.arc:arc-core-paper-menu:2.7.5")
+    implementation("ru.ruscrafting.arc:arc-core-redis:2.7.5")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
@@ -58,11 +58,11 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:6.0.7")
     testImplementation("io.kotest:kotest-assertions-core:6.0.7")
     testImplementation("io.mockk:mockk:1.14.7")
-    testImplementation("ru.ruscrafting.arc:arc-core-paper-testing:2.7.4")
-    testImplementation("ru.ruscrafting.arc:arc-core-testing:2.7.4")
+    testImplementation("ru.ruscrafting.arc:arc-core-paper-testing:2.7.5")
+    testImplementation("ru.ruscrafting.arc:arc-core-testing:2.7.5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     "integrationTestImplementation"(sourceSets.test.get().output)
-    "integrationTestImplementation"("ru.ruscrafting.arc:arc-core-integration-testing:2.7.4")
+    "integrationTestImplementation"("ru.ruscrafting.arc:arc-core-integration-testing:2.7.5")
     configurations["integrationTestImplementation"].extendsFrom(configurations["testImplementation"])
     configurations["integrationTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
 }
