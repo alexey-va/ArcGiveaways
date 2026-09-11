4b7906394ffc7d0e67e4dbe7e1fcb3abfcdf02bb
alexey-va <freedeeml@gmail.com>
2026-09-11 11:51:58 +0300

    fix: brighten native giveaway dialogue text

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ru.ruscrafting"
-version = "0.1.14"
+version = "0.1.15"
 description = "Cross-server proximity giveaways for RusCrafting"
 
 val integrationTestSourceSet = sourceSets.create("integrationTest") {
